6c4ede996c4aa60dfc2a53dbf769eba67b9ffd95
olgunbey şahin <olgunsahin0161@hotmail.com>
2023-03-01 21:03:53 +0300

    OwnedEntity

---
+++ OwnedEntityType/Migrations/20230301175403_mig1.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace OwnedEntityType.Migrations
+{
+    /// <inheritdoc />
+    public partial class mig1 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Persons",
+                columns: table => new
+                {
+                    ID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    PersonelDigerbilgiler_Adres = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    PersonelDigerbilgiler_Departman = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Persons", x => x.ID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Order",
+                columns: table => new
+                {
+                    ID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    UrunAdi = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Fiyat = table.Column<int>(type: "int", nullable: false, defaultValue: 0),
+                    OwnedEmpoyeeID = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Order", x => x.ID);
+                    table.ForeignKey(
+                        name: "FK_Order_Persons_OwnedEmpoyeeID",
+                        column: x => x.OwnedEmpoyeeID,
+                        principalTable: "Persons",
+                        principalColumn: "ID",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Order_OwnedEmpoyeeID",
+                table: "Order",
+                column: "OwnedEmpoyeeID");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Order");
+
+            migrationBuilder.DropTable(
+                name: "Persons");
+        }
+    }
+}
---
+++ OwnedEntityType/Program.cs
@@ -0,0 +1,48 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Microsoft.EntityFrameworkCore;
+
+Console.WriteLine("Hello, World!");
+AppDbContext context = new();
+List<Person> personel = context.Persons.ToList(); //OwnedEntityType'de relationship yoktur, sorgusuz sualsiz birlesik entity'de veriyi getirir
+Console.WriteLine("");
+
+
+public class Person
+{
+    public int ID { get; set; }
+    public PersonelDigerbilgiler? PersonelDigerbilgiler { get; set; }
+    public ICollection<Order> Orders { get; set; }
+
+}
+public class Order
+{
+    public string? Urun { get; set; }
+    public int Fiyat { get; set; }
+}
+public class PersonelDigerbilgiler
+{
+    public string? Adres { get; set; }
+    public string? Departman { get; set; }
+}
+public class AppDbContext:DbContext
+{
+    public DbSet<Person> Persons { get; set; }
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseSqlServer("Data Source=OLGUNPC\\SQLEXPRESS; Initial Catalog=ABC;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False");
+    }
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Person>().HasKey(p=> p.ID);
+        modelBuilder.Entity<Person>().OwnsOne(p => p.PersonelDigerbilgiler);
+        modelBuilder.Entity<Person>().OwnsMany(p => p.Orders, builder =>
+        {
+            builder.WithOwner().HasForeignKey("OwnedEmpoyeeID");
+            builder.Property<int>("ID");
+            builder.HasKey("ID");
+            builder.Property(p => p.Urun).HasColumnName("UrunAdi");
+            builder.Property(p => p.Fiyat).HasColumnName("Fiyat").HasDefaultValue(0);
+        });
+    }
+
+}
